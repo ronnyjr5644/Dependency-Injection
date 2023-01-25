9a6131179d8fc6a52b459cd8d575895c6df5bb8a
ronnyjr5644 <raunakraj20092000@gmail.com>
2023-01-25 22:50:35 +0530

    upcase 1.1

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+    'use strict';
+    angular.module('DIApp',[])
+    .controller('DIController',DIController);
+
+    function DIController ($scope,$filter, $injector){
+        $scope.name="raunak";
+        $scope.upper=function(){
+            var upCase=$filter('uppercase');
+            $scope.name=upCase($scope.name);
+        };
+        console.log($injector.annotate(DIController));
+    }
+    function annotateme(name,job,temp){
+        return "temp";
+    }
+    console.log(annotateme.toString());
+    console.log(DIController.toString());
+
+})(); 
